ff0d8c7116735cb9ad4e4add9cc71ac4618bb474
Antony Lee <anntzer.lee@gmail.com>
2018-03-13 12:32:11 -0700

    Simplify wx drawing.

--- lib/mplcairo/wx.py
+++ lib/mplcairo/wx.py
@@ -2,22 +2,13 @@ from matplotlib.backends.backend_wx import (
     _BackendWx, _FigureCanvasWxBase, FigureFrameWx, NavigationToolbar2Wx)
 import wx
 
-from . import _util, base
+from . import base
 from .base import FigureCanvasCairo
 
 
 class FigureFrameWxCairo(FigureFrameWx):
     def get_canvas(self, fig):
         return FigureCanvasWxCairo(self, -1, fig)
-
-
-# See Bitmap.FromBufferRGBA: "On Windows and Mac the RGB values will be
-# 'premultiplied' by the alpha values. (The other platforms do the
-# multiplication themselves.)
-_to_native_bitmap = (
-    _util.to_premultiplied_rgba8888
-    if wx.GetOsVersion()[0] & (wx.OS_WINDOWS | wx.OS_MAC) else
-    _util.to_unmultiplied_rgba8888)
 
 
 class FigureCanvasWxCairo(FigureCanvasCairo, _FigureCanvasWxBase):
@@ -32,10 +23,10 @@ class FigureCanvasWxCairo(FigureCanvasCairo, _FigureCanvasWxBase):
         super().draw()
         buf = self.get_renderer()._get_buffer()
         height, width, _ = buf.shape
-        self.bitmap = wx.Bitmap.FromBufferRGBA(
-            width, height, _util.to_unmultiplied_rgba8888(buf))
+        self.bitmap = wx.Bitmap(width, height, 32)
+        self.bitmap.CopyFromBuffer(buf, wx.BitmapBufferFormat_ARGB32)
         self._isDrawn = True
-        self.gui_repaint(drawDC=drawDC, origin='WXCairo')
+        self.gui_repaint(drawDC=drawDC, origin="WXCairo")
 
     def blit(self, bbox=None):
         self.draw()
