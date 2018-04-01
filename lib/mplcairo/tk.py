30baf235d4b333394fd752c0cb85f9af2f5d803b
Antony Lee <anntzer.lee@gmail.com>
2018-04-01 14:08:31 -0700

    Workaround change in backend_tk private API.

--- lib/mplcairo/tk.py
+++ lib/mplcairo/tk.py
@@ -1,9 +1,17 @@
-from matplotlib.backends import tkagg
+from functools import partial
+
 from matplotlib.backends._backend_tk import _BackendTk, FigureCanvasTk
 import numpy as np
 
 from . import _util
 from .base import FigureCanvasCairo
+
+try:
+    from matplotlib.backends._backend_tk import blit as _mpl3_blit
+    _tk_blit = partial(_mpl3_blit, offsets=(0, 1, 2, 3))
+except ImportError:
+    from matplotlib.backends.tkagg import blit as _mpl2_blit
+    _tk_blit = partial(_mpl2_blit, colormode=2)
 
 
 class FigureCanvasTkCairo(FigureCanvasCairo, FigureCanvasTk):
@@ -11,8 +19,8 @@ class FigureCanvasTkCairo(FigureCanvasCairo, FigureCanvasTk):
         super().draw()
         buf = self.get_renderer()._get_buffer()
         height, width, _ = buf.shape
-        buf = _util.to_premultiplied_rgba8888(buf)  # Empirically checked.
-        tkagg.blit(self._tkphoto, buf, colormode=2)
+        buf = _util.to_premultiplied_rgba8888(buf)
+        _tk_blit(self._tkphoto, buf)
         self._master.update_idletasks()
 
     blit = draw
