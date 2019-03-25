c0865199247a326525565b5ccff5121a83a05494
Antony Lee <anntzer.lee@gmail.com>
2019-03-25 15:03:44 +0100

    Update compositing operators example.

--- examples/operators.py
+++ examples/operators.py
@@ -3,13 +3,28 @@ mplcairo exposes all of cairo's `blending operators`_, but there is currently
 no support to set them from Matplotlib's side.  Thus, it is necessary to write
 custom artists to take advantage of this feature.
 
+One can either directly create artists of the given class (the ``OpCircle``
+example below), or replace the artist's class a posteriori with a dynamically
+generated subclass (the ``patch_class_with_operator`` example below).
+
+Note that for many operators other than the default OVER, it is necessary to
+set the figure's background to fully transparent (alpha=0) to avoid compositing
+against it.
+
 .. _blending operators: https://cairographics.org/operators/
 """
 
+import math
+
 from matplotlib import pyplot as plt
+from matplotlib.image import AxesImage
 from matplotlib.patches import Circle
+import numpy as np
 
 from mplcairo import operator_t
+
+
+ops = [*operator_t.__members__.values()]
 
 
 class OpCircle(Circle):
@@ -24,15 +39,49 @@ class OpCircle(Circle):
         gc.restore()
 
 
-ops = [operator_t.OVER,
-       operator_t.LIGHTEN,
-       operator_t.OVERLAY,
-       operator_t.SOFT_LIGHT]
-fig, axs = plt.subplots(1, len(ops))
-for op, ax in zip(ops, axs):
+fig, axs = plt.subplots(math.ceil(len(ops)**(1/2)), math.ceil(len(ops)**(1/2)))
+# The figure patch should be set to fully transparent to avoid compositing the
+# patches against it.
+fig.patch.set(alpha=0)
+for ax in axs.flat:
     ax.set_axis_off()
+for op, ax in zip(ops, axs.flat):
     ax.set_title(str(op).split(".")[-1])
-    ax.set(xlim=(-1, 2), ylim=(-1, 1), aspect="equal")
-    ax.add_artist(Circle((0, 0), 1, color="tab:blue"))
-    ax.add_artist(OpCircle((1, 0), 1, color="tab:orange", operator=op))
+    ax.set(xlim=(-1.5, 2.5), ylim=(-1.5, 1.5), aspect="equal")
+    ax.add_artist(
+        Circle((0, 0), 1, color="tab:blue", alpha=.5))
+    ax.add_artist(
+        OpCircle((1, 0), 1, color="tab:orange", alpha=.5, operator=op))
+fig.tight_layout()
+
+
+def patch_class_with_operator(artist, op):
+
+    class OpArtist(type(artist)):
+        def draw(self, renderer):
+            gc = renderer.new_gc()
+            gc.set_mplcairo_operator(op)
+            super().draw(renderer)
+            gc.restore()
+
+    artist.__class__ = OpArtist
+
+
+fig, axs = plt.subplots(math.ceil(len(ops)**(1/2)), math.ceil(len(ops)**(1/2)))
+# The figure patch should be set to fully transparent to avoid compositing the
+# patches against it.
+fig.patch.set(alpha=0)
+for ax in axs.flat:
+    ax.set_axis_off()
+for op, ax in zip(ops, axs.flat):
+    ax.set_title(str(op).split(".")[-1])
+    ax.set(xlim=(0, 5), ylim=(0, 5), aspect="equal")
+    im1 = ax.imshow(
+        np.arange(4).reshape((2, 2)), alpha=.5, extent=(0, 4, 0, 4))
+    im2 = ax.imshow(
+        np.arange(4).reshape((2, 2)), alpha=.5, extent=(1, 5, 1, 5))
+    patch_class_with_operator(im2, op)
+fig.tight_layout()
+
+
 plt.show()
