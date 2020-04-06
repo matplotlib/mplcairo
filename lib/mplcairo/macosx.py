51827b928555599a18bb0999fa37d239f6ca2e23
Greg Lucas <greg.lucas@lasp.colorado.edu>
2020-04-06 13:31:36 -0600

    change macosx backend inheritance to FigureCanvasMac for interactive event pickup.

--- lib/mplcairo/macosx.py
+++ lib/mplcairo/macosx.py
@@ -1,27 +1,12 @@
 import re
 
-from matplotlib.backends import _macosx
 from matplotlib.backends.backend_macosx import _BackendMac, FigureCanvasMac
 
 from . import _util
 from .base import FigureCanvasCairo
 
 
-class FigureCanvasMacCairo(FigureCanvasCairo, _macosx.FigureCanvas):
-    # Essentially: inherit from FigureCanvasMac without inheriting from
-    # FigureCanvasAgg.
-    locals().update({
-        k: v for k, v in vars(FigureCanvasMac).items()
-        if not re.fullmatch("__.*__", k)})
-
-    def __init__(self, figure):
-        # Inline the call to FigureCanvasCairo.__init__ as _macosx.FigureCanvas
-        # has a different signature and thus we cannot use cooperative
-        # inheritance (basically, we want that __init__, and only __init__,
-        # inserts FigureCanvasMac in the inheritance chain between
-        # FigureCanvasCairo and _macosx.FigureCanvasMac).
-        _util.fix_ipython_backend2gui()
-        FigureCanvasMac.__init__(self, figure)
+class FigureCanvasMacCairo(FigureCanvasCairo, FigureCanvasMac):
 
     # A bit hackish, but that's what _macosx.FigureCanvas wants...
     def _draw(self):
